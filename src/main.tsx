65e0335426c2ef559fc2ebb2719b86d3d585f61c
Ramil Sagidullin <rasagidullin@avito.ru>
2024-08-04 15:46:20 +0300

    feat: added metrics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { YMInitializer } from 'react-yandex-metrika';
+import App from './App.tsx';
+import { ThemeProvider } from '@/components/theme-provider.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <YMInitializer
+      accounts={[97990308]}
+      options={{
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+      }}
+    />
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
